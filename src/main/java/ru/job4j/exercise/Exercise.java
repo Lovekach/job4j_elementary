6466f8afa85329ee7d16acdf675db621089fd753
LoveKach <cryptonitik@gmail.com>
2022-04-12 15:54:32 +0300

    Update Exercise

--- src/main/java/ru/job4j/exercise/Exercise.java
+++ src/main/java/ru/job4j/exercise/Exercise.java
@@ -6,6 +6,8 @@ public class Exercise {
         System.out.println(age);
         float length = 78.0332f;
         System.out.println(length);
+        double lengthD = 78.0332d;
+        System.out.println(lengthD);
         boolean baby = true;
         System.out.println(baby);
     }
