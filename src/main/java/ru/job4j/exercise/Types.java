6c427fdca06ea50374f62a56cf96c3b1783cf27b
LoveKach <cryptonitik@gmail.com>
2022-04-12 17:39:47 +0300

    Update Exercise

---
+++ src/main/java/ru/job4j/exercise/Types.java
@@ -0,0 +1,22 @@
+package ru.job4j.exercise;
+
+public class Types {
+    public static void main(String[] args) {
+        byte age = 30;
+        System.out.println(age);
+        char gender = 'M';
+        System.out.println(gender);
+        float growth = 1.8f;
+        System.out.println(growth);
+        short bricks = 31245;
+        System.out.println(bricks);
+        int amount = 342155235;
+        System.out.println(amount);
+        int lenght = 16145472;
+        System.out.println(lenght);
+        boolean work = true;
+        System.out.println(work);
+        byte file = 12;
+        System.out.println(file);
+    }
+}
