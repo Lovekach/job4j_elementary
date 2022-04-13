a001b9bfcd0fc9a48a4cd2efcede8cb69dc223b6
LoveKach <cryptonitik@gmail.com>
2022-04-13 15:35:08 +0300

    Types repeat

---
+++ src/main/java/ru/job4j/exercise/Types2.java
@@ -0,0 +1,22 @@
+package ru.job4j.exercise;
+
+public class Types2 {
+    public static void main(String[] args) {
+        int age = 23;
+        System.out.println(age);
+        char man = 'M';
+        System.out.println(man);
+        float min = 1.8f;
+        System.out.println(min);
+        short bricks = 31453;
+        System.out.println(bricks);
+        double bacteria = 18234858;
+        System.out.println(bacteria);
+        long ml = 789393;
+        System.out.println(ml);
+        boolean job = true;
+        System.out.println(job);
+        byte file = 17;
+        System.out.println(file);
+    }
+}
