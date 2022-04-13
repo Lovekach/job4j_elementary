b637df6ea896c32cfd830b15b41139274d343156
LoveKach <cryptonitik@gmail.com>
2022-04-13 17:15:57 +0300

    Calculator2.java repeat and learn

---
+++ src/main/java/ru/job4j/calculate/Calculator2.java
@@ -0,0 +1,36 @@
+package ru.job4j.calculate;
+
+public class Calculator2 {
+    public static void main(String[] args) {
+        int four = 4;
+        int seven = 7;
+        int ten = 10;
+        int tenPlusSeven = 10 + 7;
+        System.out.println(tenPlusSeven);
+        int sevenMinusFour = 7 - 4;
+        System.out.println(sevenMinusFour);
+        int five = 10;
+        int one = 2;
+        int fiveDivone = 10/2;
+        System.out.println(fiveDivone);
+        int age = 18;
+        System.out.println(age);
+        age = 19;
+        System.out.println(age);
+        age = 3;
+        System.out.println(age);
+        int look = 34;
+        look = look + 3;
+        System.out.println(look);
+
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
+
+
+
+
+    }
+
+}
