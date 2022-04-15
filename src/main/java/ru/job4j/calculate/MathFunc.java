a7fbab0dbc4879d18725227d966cc66c6b9c9a29
LoveKach <cryptonitik@gmail.com>
2022-04-15 20:12:01 +0300

    ArgMethod

--- src/main/java/ru/job4j/calculate/MathFunc.java
+++ src/main/java/ru/job4j/calculate/MathFunc.java
@@ -1,18 +1,17 @@
 package ru.job4j.calculate;
 
 public class MathFunc {
-    public static int funch1(int x){
+    public static int func1(int x){
         int y = x * x + 1;
         return y;
     }
-    public static int funch2(int x){
+    public static int func2(int x){
         int y = 1 / x;
         return y;
     }
     public static void main(String[] args) {
-       int result1 = MathFunc.funch1(3);
-       result1 = MathFunc.funch1(100);
-       int result2 = MathFunc.funch2(5);
+       int result1 = MathFunc.func1(100);
+       int result2 = MathFunc.func2(5);
        int result3 = result1 + result2;
         System.out.println(result3);
     }
