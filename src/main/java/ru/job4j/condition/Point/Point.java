8a600cde0d978800703c0bba37429b146478de70
LoveKach <cryptonitik@gmail.com>
2022-04-18 20:51:59 +0300

    Point.java

---
+++ src/main/java/ru/job4j/condition/Point/Point.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition.Point;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double rsl = -1;
+        return rsl;
+    }
+    public static double distance; {
+        double first = 2 - 0;
+        double second = 0 - 0;
+        double three = Math.pow(first, 2);
+        double four = Math.pow(second, 2);
+        double five = three + four;
+        double result = Math.sqrt(five);
+
+    }
+
+    public static void main(String[] args) {
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+
+
+    }
+}
