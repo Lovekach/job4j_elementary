54fa991f2dc5110b1367d5fbce8886e2cc6f4a02
LoveKach <cryptonitik@gmail.com>
2022-05-19 18:21:31 +0300

    Repeat under epidemic(calculator)

---
+++ src/main/java/ru/job4j/calculate/Calculator.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculate;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int tree = 3;
+        int nine = 9;
+        int ten = 10;
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+        int fourTimeTwo = four * two;
+        System.out.println(fourTimeTwo);
+
+    }
+}
