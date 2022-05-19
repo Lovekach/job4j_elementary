ff21c1ca6a6ed64cf0cc66289a37b7f2e6b0d829
LoveKach <cryptonitik@gmail.com>
2022-05-19 19:03:57 +0300

    Repeat under epidemic(calculator2)

---
+++ src/main/java/ru/job4j/exercise/Calculator2.java
@@ -0,0 +1,18 @@
+package ru.job4j.exercise;
+
+public class Calculator2 {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        Calculator2.plus(1, 2);
+        Calculator2.plus(10, 11);
+        Calculator2.plus(100, 500);
+        Calculator2.plus(4, 2);
+        Calculator2.plus(3, 5);
+    }
+
+
+}
