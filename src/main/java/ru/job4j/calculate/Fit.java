326994ab7fe1f3cb061a2929ac186b1e7404a698
LoveKach <cryptonitik@gmail.com>
2022-05-21 20:06:08 +0300

    Repeat under epidemic(Fit man and woman)

--- src/main/java/ru/job4j/calculate/Fit.java
+++ src/main/java/ru/job4j/calculate/Fit.java
@@ -2,7 +2,7 @@ package ru.job4j.calculate;
 
 public class Fit {
     public static double manWeight(short height) {
-        double rsl = (187 - 100) * 1.15;
+        double rsl = (181 - 100) * 1.15;
         return rsl;
     }
     public static double womanWeight(short height) {
@@ -11,9 +11,9 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short height = 187;
+        short height = 181;
         double man = Fit.manWeight(height);
-        System.out.println("Man 187 is " + man);
+        System.out.println("Man 181 is " + man);
         height = 162;
         double woman = Fit.womanWeight(height);
         System.out.println("Woman 162 is " + woman);
