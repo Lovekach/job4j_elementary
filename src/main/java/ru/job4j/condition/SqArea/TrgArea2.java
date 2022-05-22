68aec9284c489082028a161f36deed669801c1d8
LoveKach <cryptonitik@gmail.com>
2022-05-22 19:38:02 +0300

    Repeat at material before epidemic(TrgArea2)

---
+++ src/main/java/ru/job4j/condition/SqArea/TrgArea2.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition.SqArea;
+
+public class TrgArea2 {
+    public static double area(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        double first = p - a;
+        double second = p - b;
+        double third = p - c;
+        double four = p * (first * second * third);
+        double five = Math.sqrt(p * four);
+        return five;
+
+    }
+
+    public static void main(String[] args) {
+        double rsl = TrgArea2.area(2,2,2);
+        System.out.println("area (2, 2, 2) + " + rsl);
+
+    }
+
+
+}
