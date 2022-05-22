d036bb462eb3f8ffb23a4e47323be3dadf94ec39
LoveKach <cryptonitik@gmail.com>
2022-05-22 19:52:42 +0300

    Repeat at material before epidemic(Greeting2)

---
+++ src/main/java/ru/job4j/condition/Greeting2.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Greeting2 {
+    public static void main(String[] args) {
+        String idea = "I Like Java";
+        System.out.println(idea);
+        String doc = "But I am a newbie";
+        System.out.println(idea + doc);
+        int year = 2022;
+        String separate = " ";
+        System.out.println(idea + doc + separate + year);
+
+
+    }
+}
