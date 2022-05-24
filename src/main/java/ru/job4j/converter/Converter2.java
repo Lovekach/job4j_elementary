37a8bebae3036ef5a3c7b22f1a0e383224ebdd3a
LoveKach <cryptonitik@gmail.com>
2022-05-24 19:22:00 +0300

    Testing Converter2 value

---
+++ src/main/java/ru/job4j/converter/Converter2.java
@@ -0,0 +1,22 @@
+package ru.job4j.converter;
+
+public class Converter2 {
+    public static float rubleToEuro(float value) {
+        return value / 70;
+    }
+    public static float rubleToDollar(float value) {
+        return value / 60;
+
+    }
+
+    public static void main(String[] args) {
+        float in  = 140;
+        float expected = 2;
+        float out = Converter2.rubleToEuro(in);
+        float out2 = Converter2.rubleToDollar(in);
+        boolean passed = expected == out;
+        boolean passed2 = expected == out2;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        System.out.println("140 rubles are 2. Test result : " + passed2);
+    }
+}
