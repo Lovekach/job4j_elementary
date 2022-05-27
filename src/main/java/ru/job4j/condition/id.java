7921639acd6c41b711bab689701edc97a6a170d2
LoveKach <cryptonitik@gmail.com>
2022-05-27 21:01:14 +0300

    Import And Test assertEquals

--- src/main/java/ru/job4j/condition/id.java
+++ src/main/java/ru/job4j/condition/id.java
@@ -1,24 +1,24 @@
-package ru.job4j.condition;
-
-public class id {
-    public static void main(String[] args) {
-        long l = 129;
-        byte b = (byte) 1;
-        System.out.println("Результат вычисления:" + b);
-        float f = 12358.7f;
-        int i = 45981;
-        short rsl = (short) (f + i);
-        System.out.println("Результат вычисления:" + rsl);
-        char c = 45000;
-        float i = c;
-        System.out.println("Результат вычисления:" + i);
-        double d = 121.19;
-        byte b = (byte) d;
-        System.out.println("Результат вычисления:" + b);
-        short s = 1500;
-        char c = (char) s;
-        double d = c;
-        System.out.println("Результат вычисления:" + d);
-
-    }
-}
+//package ru.job4j.condition;
+//
+//public class id {
+//    public static void main(String[] args) {
+//        long l = 129;
+//        byte b = (byte) 1;
+//        System.out.println("Результат вычисления:" + b);
+//        float f = 12358.7f;
+//        int i = 45981;
+//        short rsl = (short) (f + i);
+//        System.out.println("Результат вычисления:" + rsl);
+//        char c = 45000;
+//        float i = c;
+//        System.out.println("Результат вычисления:" + i);
+//        double d = 121.19;
+//        byte b = (byte) d;
+//        System.out.println("Результат вычисления:" + b);
+//        short s = 1500;
+//        char c = (char) s;
+//        double d = c;
+//        System.out.println("Результат вычисления:" + d);
+//
+//    }
+//}
