53db816eb3e391da2626f035bea4a4d5919cc891
LoveKach <cryptonitik@gmail.com>
2022-06-23 15:24:32 +0300

    Ternarnoe uslovie dorabotka

--- src/main/java/ru/job4j/condition/Film1.java
+++ src/main/java/ru/job4j/condition/Film1.java
@@ -7,8 +7,17 @@ public class Film1 {
 
     }
 
+    public static String mana(int age) {
+        String label = age >= 18 ? "Yes bro." : "Sorry bro, u can't do it.";
+        return label;
+
+
+    }
+
     public static void main(String[] args) {
         String msg = Film1.permission(33);
         System.out.println(msg);
+        String mdd = Film1.mana(14);
+        System.out.println(mdd);
     }
 }
