59197840ce4d8eb09bcabcc5d39f4e38f702f014
LoveKach <cryptonitik@gmail.com>
2022-06-24 19:51:33 +0300

    if with return PointEq1

--- src/main/java/ru/job4j/condition/PointEq1.java
+++ src/main/java/ru/job4j/condition/PointEq1.java
@@ -8,5 +8,6 @@ public class PointEq1 {
     public static void main(String[] args) {
         System.out.println(eq(1, 1, 2, 2, 3, 3));
         System.out.println(eq(10, 10, 10, 10, 10, 10));
+
     }
 }
