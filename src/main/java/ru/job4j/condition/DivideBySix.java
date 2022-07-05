718146b5a846479c8b44e0103504292da0ba2a65
LoveKach <cryptonitik@gmail.com>
2022-07-05 20:35:25 +0300

    DivideBySix

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -3,17 +3,28 @@ package ru.job4j.condition;
 public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
-        if (number % 3 == 0) {
-            if (number % 2 == 0) {
-                rsl = "Исходное число делится на 6.";
-            } else {
-                rsl = "Исходное число делится на 3, но не является четным.";
-            }
+        if (number % 3 == 0 && number % 2 == 0) {
+            rsl = "Исходное число делится на 6.";
+        } else if (number % 3 == 0) {
+            rsl = "Исходное число делится на 3, но не является четным.";
+        } else if (number % 3 != 0) {
+            rsl = "Исходное число не делится на 3, но является четным.";
         } else {
-            if (number % 2 == 0) {
-                rsl = "Исходное число не делится на 3, но является четным.";
-            } else {
-                rsl = "Исходное число не делится на 3 и не является четным,";
-            }
+            rsl = "Исходное число не делится на 3 и не является четным,";
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        int rsl = 24 % 6;
+        System.out.println("Остаток от деления числе 24 и 6 равен: " + rsl);
+        int rsl1 = 9 % 3;
+        System.out.println("Остаток деления чисел 9 и 3 равен: " + rsl1);
+        int rsl2 = 14 % 3;
+        System.out.println("Остаток от деления числе 14 и 3 равен: " + rsl2);
+        int rsl3 = 25 % 3;
+        System.out.println("Остаток от деления числе 25 и 3 равен: " + rsl3);
+
+    }
+}
+
